88a6f165072af9588b0df5ac176792a4a5ab3c12
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-23 08:21:22 +0900

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+console.log('\'Allo \'Allo!');
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks2',
+    image: './images/vmSocks-green.png',
+    url: 'https://masan-s.com/puki',
+    inStock: true
+  }
+})
