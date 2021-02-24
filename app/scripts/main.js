8462e2dc1afea0062f84bba1e8e662ffb34597c9
ma <ma_yamaki@yahoo.com>
2021-02-24 14:10:10 +0900

    dd

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,25 @@ var app = new Vue({
     product: 'Socks2',
     image: './images/vmSocks-green.png',
     url: 'https://masan-s.com/puki',
-    inStock: true
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Grender-neutral"],
+    cart: 0,
+    variants: [{
+      variantId: 2234,
+      variantColor: "blue",
+      variantImage: './images/vmSocks-blue.png'
+    }, {
+      variantId: 2235,
+      variantColor: "green",
+      variantImage: './images/vmSocks-green.png'
+    }]
+  },
+  methods: {
+    addToCart: function(){
+      this.cart +=1
+    },
+    updateProduct: function(variantImage){
+      this.image = variantImage
+    }
   }
 })
